c9d6e256a70d2d1889766e0bd0f1b6807ed79c2c
omerkocbil <omer-95@hotmail.com>
2022-01-26 19:57:58 +0300

    feat: add statistical imputation guideline with sklearn
    Add examples of statistical imputation with scikit-learn library on
    missing_value module. Use SimpleImputer class for this missing value
    imputation. Examples contain mean for 'mean' strategy, median for
    'median' strategy and mode for 'most_frequent' strategy calculations.

--- missing_value/stats.py
+++ missing_value/stats.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
 
 
 #define dataframe
@@ -12,3 +14,16 @@ house_price['LotFrontage'] = house_price['LotFrontage'].fillna(house_price['LotF
 
 #fill nan values with mode value - pandas
 house_price['GarageYrBlt'] = house_price['GarageYrBlt'].fillna(house_price['GarageYrBlt'].mode()[0])
+
+
+#fill nan values with mean value - sklearn
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+house_price['LotFrontage'] = imputer.fit_transform(house_price['LotFrontage'].values.reshape(len(house_price['LotFrontage'].values), 1))
+
+#fill nan values with median value - sklearn
+imputer = SimpleImputer(missing_values=np.nan, strategy='median')
+house_price['LotFrontage'] = imputer.fit_transform(house_price['LotFrontage'].values.reshape(len(house_price['LotFrontage'].values), 1))
+
+#fill nan values with mode value - sklearn
+imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+house_price['GarageYrBlt'] = imputer.fit_transform(house_price['GarageYrBlt'].values.reshape(len(house_price['GarageYrBlt'].values), 1))
