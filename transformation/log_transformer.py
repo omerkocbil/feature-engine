49067b4f8a1db588629e64902c765751b554395e
omerkocbil <omer-95@hotmail.com>
2022-01-26 20:13:02 +0300

    feat: add log transformation guideline with sklearn
    Add examples of log transformation with scikit-learn library on
    transformation module. Use FunctionTransformer class and np.log argument
    to transformation.

--- transformation/log_transformer.py
+++ transformation/log_transformer.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import FunctionTransformer
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -32,3 +34,7 @@ plt.title('Age PDF')
 plt.subplot(122)
 stats.probplot(titanic['Fare'], dist="norm", plot=plt)
 plt.title('Age QQ Plot')
+
+#log transformations - sklearn
+transformer = FunctionTransformer(func=np.log1p)
+titanic['Fare'] = transformer.fit_transform(titanic['Fare'])
