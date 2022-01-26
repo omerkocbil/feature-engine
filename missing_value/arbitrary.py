92a3ed911ab35d4365cb2b6dd4403ca773df526d
omerkocbil <omer-95@hotmail.com>
2022-01-26 19:50:16 +0300

    feat: add arbitrary imputation guideline with feature-engine
    Add examples of arbitrary imputation with feature-engine library on
    missing_value module. Use ArbitraryNumberImputer class for this
    missing value imputation.

--- missing_value/arbitrary.py
+++ missing_value/arbitrary.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
+from feature_engine.imputation import ArbitraryNumberImputer
 
 
 #define dataframe
@@ -13,3 +14,16 @@ house_price['GarageYrBlt'] = house_price['GarageYrBlt'].fillna(-1)
 #fill nan values with arbitrary value - sklearn
 imputer = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=-1)
 house_price['GarageYrBlt'] = imputer.fit_transform(house_price['GarageYrBlt'].values.reshape(len(house_price['GarageYrBlt'].values), 1))
+
+#fill nan values with arbitrary value - feature_engine
+imputer = ArbitraryNumberImputer(arbitrary_number=-999,
+                                 variables=['LotFrontage', 'MasVnrArea'])
+imputer.fit(house_price)
+imputer.arbitrary_number
+imputer.variables_
+house_price = imputer.transform(house_price)
+
+#fill nan values with arbitrary value with different values - feature_engine
+imputer = ArbitraryNumberImputer(imputer_dict={'LotFrontage': -999, 'MasVnrArea': -999, 
+                                               'GarageYrBlt': -1})
+house_price = imputer.fit_transform(house_price)
