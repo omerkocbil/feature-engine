58b87a09ed7c301eb3e4f82afe3e97d44aaca79e
omerkocbil <omer-95@hotmail.com>
2022-01-26 19:48:14 +0300

    feat: add arbitrary imputation guideline with sklearn
    Add examples of arbitrary imputation with scikit-learn library on
    missing_value module. Use SimpleImputer class for this imputation.

--- missing_value/arbitrary.py
+++ missing_value/arbitrary.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
 
 
 #define dataframe
@@ -7,3 +9,7 @@ house_price = pd.read_csv('../sample_datasets/house_price.csv')
 #fill nan values with arbitrary value - pandas
 house_price['GarageCond'] = house_price['GarageCond'].fillna('None')
 house_price['GarageYrBlt'] = house_price['GarageYrBlt'].fillna(-1)
+
+#fill nan values with arbitrary value - sklearn
+imputer = SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=-1)
+house_price['GarageYrBlt'] = imputer.fit_transform(house_price['GarageYrBlt'].values.reshape(len(house_price['GarageYrBlt'].values), 1))
