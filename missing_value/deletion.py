08bf097d3439e040c0ce938fd64d561869839f7d
omerkocbil <omer-95@hotmail.com>
2022-01-26 19:13:31 +0300

    feat: add drop by examples with pandas
    Add examples of dropping by rows and columns with pandas library on
    missing_value module.

---
+++ missing_value/deletion.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+#define dataframe
+house_price = pd.read_csv('../sample_datasets/house_price.csv')
+
+#delete by rows
+house_price = house_price.dropna(axis=0)
+house_price = house_price.dropna(axis='rows')
+
+#delete by columns
+house_price = house_price.dropna(axis=1)
+house_price = house_price.dropna(axis='columns')
+
+#delete with permanently
+house_price.dropna(axis=1, inplace=True)
+
+#delete by row's and column's nan count
+house_price = house_price.dropna(how='all')
+house_price = house_price.dropna(how='any')
+house_price = house_price.dropna(how='any', axis=1)
+
+#delete by row's and column's thresh non-NA count
+house_price = house_price.dropna(thresh=75)
+house_price = house_price.dropna(thresh=75, axis=1)
+
+#delete by only selected columns
+house_price = house_price.dropna(subset=['PoolQC'])
+
+#delete specific column
+house_price = house_price.drop(['PoolQC'], axis=1)
+house_price = house_price.drop(columns=['PoolQC'])
+house_price.drop(columns=['PoolQC'], inplace=True)
+
+#delete specific row
+house_price = house_price.drop(index=[0, 1])
+house_price.drop(index=[0, 1], inplace=True)
